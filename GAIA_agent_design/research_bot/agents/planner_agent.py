94979aa91886409e20ab5155ec8647205becc8ca
David (Yun) Du <davidduyun@gmail.com>
2025-07-10 22:28:20 +0000

    v4.1.2

--- GAIA_agent_design/research_bot/agents/planner_agent.py
+++ GAIA_agent_design/research_bot/agents/planner_agent.py
@@ -5,7 +5,7 @@ from ...agents_lib.tools import transcribe_audio
 
 PROMPT = (
     "You are a helpful leading research assistant. Given a question "
-    "and optional context, come up with a corresponding set of queries that closely adhere to the original question "
+    "and optional context, come up with a corresponding set of queries that unbiasedly and closely adhere to the original question "
     "to perform to best answer the question. For each query, the source can be either 'context' or 'web'. The queries are going to be distributed to a group of research assistants (one person per question),"
     "so make sure every query is clear and concise with enough background information from the question and no dependencies on other queries. Each query should be as concise and relevant to the original question as possible. First check if the "
     "context likely contains the answer. If so, plan to search or analyze that "
--- GAIA_agent_design/research_bot/agents/writer_agent.py
+++ GAIA_agent_design/research_bot/agents/writer_agent.py
@@ -5,12 +5,13 @@ from agents import Agent
 PROMPT = (
     "You are a world-class professor. You will be given the original question, any context "
     "from media files, and summaries of research your research assistants have provided. "
-    "Reason about the provided information and the question provided carefully, "
+    "Reason about the provided information and the question provided carefully and step by step in detail, "
     "then finish your answer. You must follow the structured output format. Put your answer in the \"answer\" field and your reason in the \"reasoning\" field. "
     "Your answer should be a number OR as few words as possible OR a comma separated list of numbers and/or strings. "
     "If you are asked for a number, don't use comma to write your number neither use units such as $ or percent sign unless specified otherwise. \
-    If you are asked for a string, don't use articles, neither abbreviations (e.g. for cities), and write the digits in plain text unless specified otherwise. \
+    If you are asked for a string, don't use articles (unless it is contained in a specifc phrase), neither abbreviations (e.g. for cities), and write the digits in plain text unless specified otherwise. \
     If you are asked for a comma separated list, apply the above rules depending of whether the element to be put in the list is a number or a string."
+    " Before handing over your answer, check units, rounding etc. per the original question's instructions."
 )
 
 
